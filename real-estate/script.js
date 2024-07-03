c22fa63640abdbd1024cc8e86439bcf3a6459cfd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-02 21:00:31 -0400

    Use querySelector to grab price range IDs for improved efficiency.

---
+++ real-estate/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const rangeInput = document.querySelector('#price-range');
+  const rangeValue = document.querySelector('#range-value');
+})
