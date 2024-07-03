960a41d56e044062b43927644683efd0f4556dd1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-02 21:07:13 -0400

    Formatted code for consistency using prettier code.

--- real-estate/script.js
+++ real-estate/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const rangeInput = document.querySelector('#price-range');
   const rangeValue = document.querySelector('#range-value');
-  
-  //Initalize the display element with the current range value. 
+
+  //Initalize the display element with the current range value.
   rangeValue.textContent = rangeInput.value;
 
   // Add an event listener to update the display element when the range value changes
-  rangeInput.addEventListener('input', function() {
+
+  rangeInput.addEventListener('input', function () {
     rangeValue.textContent = rangeInput.value;
   });
-
 });
