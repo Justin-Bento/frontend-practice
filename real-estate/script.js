acc613f77e11dec98196bee496cb9558e2abcc2b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-02 21:04:46 -0400

    Initalized the display element with the current range value.

--- real-estate/script.js
+++ real-estate/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   const rangeInput = document.querySelector('#price-range');
   const rangeValue = document.querySelector('#range-value');
+  
+  //Initalize the display element with the current range value. 
+  rangeValue.textContent = rangeInput.value;
+
 });
