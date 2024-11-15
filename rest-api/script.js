17707599712e4cdc95c5970ed0ecba44d695a47c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-11-15 11:25:58 -0500

    Added a const that creates a paragraph element

--- rest-api/script.js
+++ rest-api/script.js
@@ -1,5 +1,8 @@
 const mainApp = document.querySelector("#app");
 
+// Creating a new HTML Elements
+const summary = document.createElement("p");
+
 fetch("https://dummyjson.com/products/1")
   .then((res) => res.json())
   .then((req) => (mainApp.textContent = req.title))
