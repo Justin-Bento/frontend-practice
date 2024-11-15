8d00effe333650d3581ade14bddf580d194a49b0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-11-15 11:27:29 -0500

    Added curly braces inside the request header to contains multiple statements

--- rest-api/script.js
+++ rest-api/script.js
@@ -5,5 +5,8 @@ const summary = document.createElement("p");
 
 fetch("https://dummyjson.com/products/1")
   .then((res) => res.json())
-  .then((req) => (mainApp.textContent = req.title))
+  .then((req) => ({
+    // Set the product title as the main content of #app
+    mainApp.textContent = req.title
+  }))
   .catch((error) => console.error("Error fetching product:", error));
