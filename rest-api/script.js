5133b62c615e897fc13203031e9b3ff184fd8cbf
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-11-15 11:21:51 -0500

    Added catch error if the fetch api fails.

--- rest-api/script.js
+++ rest-api/script.js
@@ -2,4 +2,5 @@ const mainApp = document.querySelector("#app");
 
 fetch("https://dummyjson.com/products/1")
   .then((res) => res.json())
-  .then((req) => (mainApp.textContent = req.title));
+  .then((req) => (mainApp.textContent = req.title))
+  .catch((error) => console.error("Error fetching product:", error));
